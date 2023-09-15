95be69c7370cd2815642750e29c1c9cb8fa6211e
usuario <usuario@usuario-PC>
2023-09-15 16:11:48 -0500

    ejercicio 18

---
+++ src/ejercicio18/Ejercicio18.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio18;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ejercicio18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       main ventana = new main();
+       ventana.setVisible(true);
+    }
+    
+}
