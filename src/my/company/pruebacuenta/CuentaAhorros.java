28152462855b236f1193c1f5271d42d436ba0890
usuario <usuario@usuario-PC>
2023-10-10 15:56:52 -0500

    Ejercicio

---
+++ src/my/company/pruebacuenta/CuentaAhorros.java
@@ -0,0 +1,29 @@
+
+package my.company.pruebacuenta;
+
+
+public class CuentaAhorros extends Cuenta {
+    private boolean activa;
+
+public CuentaAhorros(float saldo, float tasa) {
+super(saldo, tasa);
+if (saldo < 10000) 
+activa = false; else
+activa = true; }
+   
+public void retirar(float cantidad) {
+if (activa) 
+super.retirar(cantidad); }
+public void consignar(float cantidad) {
+if (activa) 
+super.consignar(cantidad); 
+}
+
+ public void imprimir() {
+     System.out.println("Saldo = $ " + saldo);
+     System.out.println("Comisión mensual = $ " + comisiónMensual);
+      System.out.println("Número de transacciones = " +(númeroConsignaciones + númeroRetiros));
+      System.out.println();
+    }
+
+}
